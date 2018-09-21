c9b9379f6981a44023130160db517739cf9bafc2
Stefan van der Walt <stefanv@berkeley.edu>
2018-09-21 11:12:14 -0700

    Remove Terraform and Ansible scripts in preparation for GCP deployment

--- init-instance.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-yum -y update
-yum install -y git
-pip install ansible
-export PATH=$PATH:/usr/local/bin
-export ANSIBLE_PULL="ansible-pull -U https://github.com/skyportal/deploy \
-                     --extra-vars=\"variable_host=localhost\" -i \"localhost,\""
-$ANSIBLE_PULL playbooks/install-deps.yaml
-$ANSIBLE_PULL playbooks/deploy-app.yaml
